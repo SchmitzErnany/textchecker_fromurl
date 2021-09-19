34afea2cca1ad0ac3e82ee2f9b1c660a9959b5f4
SchmitzErnany <ernany0@gmail.com>
2021-09-19 20:05:10 -0300

    diff_outputs.py modified

--- diff_outputs.py
+++ diff_outputs.py
@@ -1,51 +1,29 @@
+#%%
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 27 14:09:11 2020
-
-@author: ernany
-"""
 
 import pandas as pd
-first_temp = pd.read_excel('6k+_temp108beforedeploy_data.xlsx')
-second_temp = pd.read_excel('6k+_temp109beforedeploy_data.xlsx')
 
-#%%
+columns_names = [
+    "correct_sentence",
+    "changed_sentence",
+    "iterations",
+    "positive",
+]
+# defining the dataframes that will be compared
+first_temp = pd.read_csv("output_diff_comma08_all08.csv", sep="\t", names=columns_names)[['correct_sentence']]
+second_temp = pd.read_csv("output_diff_comma09_all08.csv", sep="\t", names=columns_names)[['correct_sentence']]
 
-first_temp_fail = first_temp[first_temp['FAIL'].notna()]['Analyzed text'].to_frame()
-second_temp_fail = second_temp[second_temp['FAIL'].notna()]['Analyzed text'].to_frame()
-
-diff = second_temp_fail.merge(first_temp_fail, how = 'outer', indicator=True).loc[lambda x : x['_merge']=='left_only']
+# difference between both dataframes
+diff = first_temp.merge(second_temp, how="outer", indicator=True).loc[
+    lambda x: x["_merge"] == "left_only"
+]
 diff = diff.drop_duplicates()
-
-joined = ''
-for para in diff['Analyzed text'].values:
-    joined += para + '\n'
-    
-joined
-
-#%%
-
-#first_temp_fail = first_temp[first_temp['Misspelling'].notna()]['Analyzed text'].to_frame()
-#second_temp_fail = second_temp[second_temp['Misspelling'].notna()]['Analyzed text'].to_frame()
-#
-#diff = second_temp_fail.merge(first_temp_fail, how = 'outer', indicator=True).loc[lambda x : x['_merge']=='left_only']
-#diff = diff.drop_duplicates()
-#
-#joined = ''
-#for para in diff['Analyzed text'].values:
-#    joined += para + '\n'
-#    
-#joined
-
-
-#%%
-
-#file_df = pd.read_csv('frases_problem.txt', sep='\n', names=['sentence'])
-#unique_df = file_df.drop_duplicates()
-#
-#joined = ''
-#for para in unique_df['sentence'].values:
-#    joined += para + '\n'
-#    
-#joined
+diff
+# %%
+diff['correct_sentence'].iloc[201]
+# %%
+example = pd.read_csv("output_diff_comma08_addcrase06_all08.csv", sep="\t", names=columns_names)[['changed_sentence']]
+# %%
+example['changed_sentence'].iloc[100]
+# %%
